b23dee552f070e461e9e8fa607cb045d93ba01fd
aishwxryag <106765136+aishwxryag@users.noreply.github.com>
2022-06-02 22:10:45 +0530

    Create RadiuOfCircle.py

---
+++ RadiuOfCircle.py
@@ -0,0 +1,8 @@
+#Program to print area of circle by taking radius as input
+radius = input("Enter radius of circle: ")
+area = 3.14 * float(radius) * float(radius)
+print("The area of the circle with radius " + str(radius) + " is: " + str(area))
+
+"""Output:
+Enter radius of circle: 1.1
+The area of the circle with radius 1.1 is: 3.7994000000000008"""
